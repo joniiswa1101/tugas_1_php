1e7f06e8663f89c24c26b630a98c654db054e8f4
Joni Iswanto <joniiswa1101@gmail.com>
2019-10-03 15:54:48 +0700

    Tugas 1 PHP
    echo & print

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+  echo "<p><h1>Daftar Menu</h1></p>";
+  echo "<br>\n";
+  echo "<ul>\n";
+  echo "<li> Ayam Goreng  = Rp 13.000";
+  echo "<li> Ayam Bakar   = Rp 15.000";
+  echo "<li> Ayam Sayur   = Rp 13.000";
+  echo "<li> Es Teh       = Rp 15.000";
+  echo "<li> Tempe Goreng = Rp 1.000";
+  echo "<li> Tahu Goreng  = Rp 1.000";
+  echo "<li> Soto         = Rp 3.000";
+  echo "<li> Nasi Putih   = Rp 15.000";
+  echo "<li> Es Tebu      = Rp 5.000";
+  echo "</ul>\n";
+
+?>
